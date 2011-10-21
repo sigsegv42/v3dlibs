13d9eb556a2bf83023429dc7501ff572207eda27
Josh Farr <j.wgasa@gmail.com>
2011-10-21 00:25:33 -0700

    fix coverage reporting for image library

--- build-code-coverage-report.sh
+++ build-code-coverage-report.sh
@@ -5,6 +5,20 @@
 # ./configure --enable-gcov
 #
 
+if [ ! -f src/image/readers/readers ]
+then
+  cd src/image/readers
+  ln -nsf . readers
+  cd ../../../
+fi
+
+if [ ! -d src/image/writers/writers ]
+then
+  cd src/image/writers
+  ln -nsf . writers
+  cd ../../../
+fi
+
 make gcov-clean
 make gcov-reset
 make test
