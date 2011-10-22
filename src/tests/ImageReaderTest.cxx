8baa3eca7a7a24b4e87ce8f3f9905eb9c6118504
Josh Farr <j.wgasa@gmail.com>
2011-10-21 18:40:06 -0700

    add image reader/writer test files

---
+++ src/tests/ImageReaderTest.cxx
@@ -0,0 +1,15 @@
+#include <boost/test/unit_test.hpp>
+
+#include "../image/ImageFactory.h"
+
+BOOST_AUTO_TEST_CASE( imagereader_test )
+{
+	v3D::ImageFactory factory;
+
+	std::string bmpFilename("data/2x2x24_red.bmp");
+	boost::shared_ptr<v3D::Image> image = factory.read(bmpFilename);
+	BOOST_CHECK_EQUAL(image->width(), 2);
+	BOOST_CHECK_EQUAL(image->height(), 2);
+	BOOST_CHECK_EQUAL(image->bpp(), 24);
+	BOOST_CHECK_EQUAL((*image)[0], 0);
+}
---
+++ src/tests/ImageWriterTest.cxx
@@ -0,0 +1,7 @@
+#include <boost/test/unit_test.hpp>
+
+#include "../image/Image.h"
+
+BOOST_AUTO_TEST_CASE( imagewriter_test )
+{
+}
