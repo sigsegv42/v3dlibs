737973867d8ed1cee8ea14e49754b339be57d663
Josh Farr <j.wgasa@gmail.com>
2011-10-21 19:07:26 -0700

    fix test

--- src/tests/ImageReaderTest.cxx
+++ src/tests/ImageReaderTest.cxx
@@ -11,5 +11,7 @@ BOOST_AUTO_TEST_CASE( imagereader_test )
 	BOOST_CHECK_EQUAL(image->width(), 2);
 	BOOST_CHECK_EQUAL(image->height(), 2);
 	BOOST_CHECK_EQUAL(image->bpp(), 24);
-	BOOST_CHECK_EQUAL((*image)[0], 0);
+	BOOST_CHECK_EQUAL((*image)[0], 0xff);
+	BOOST_CHECK_EQUAL((*image)[1], 0);
+	BOOST_CHECK_EQUAL((*image)[2], 0);
 }
