54393d89a68d7b9cd1ec818eacab760a224449c9
Josh Farr <j.wgasa@gmail.com>
2011-10-29 16:20:33 -0700

    global test fixture to initialize logger + link against logger library

--- src/tests/ImageReaderTest.cxx
+++ src/tests/ImageReaderTest.cxx
@@ -14,4 +14,14 @@ BOOST_AUTO_TEST_CASE( imagereader_test )
 	BOOST_CHECK_EQUAL((*image)[0], 0xff);
 	BOOST_CHECK_EQUAL((*image)[1], 0);
 	BOOST_CHECK_EQUAL((*image)[2], 0);
+
+	std::string jpgFilename("data/2x2x24_green.jpg");
+	boost::shared_ptr<v3D::Image> jpgImage = factory.read(jpgFilename);
+	BOOST_CHECK_EQUAL(jpgImage->width(), 2);
+	BOOST_CHECK_EQUAL(jpgImage->height(), 2);
+	BOOST_CHECK_EQUAL(jpgImage->bpp(), 24);
+	BOOST_CHECK_EQUAL((*jpgImage)[0], 0);
+	BOOST_CHECK_EQUAL((*jpgImage)[1], 0xff);
+	BOOST_CHECK_EQUAL((*jpgImage)[2], 0x1);
+
 }
--- src/tests/TestMain.cxx
+++ src/tests/TestMain.cxx
@@ -1,3 +1,23 @@
 #define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MODULE Master Test Suite
 #include <boost/test/unit_test.hpp>
+
+// include log4cxx header files.
+#include "log4cxx/logger.h"
+#include "log4cxx/basicconfigurator.h"
+#include "log4cxx/helpers/exception.h"
+
+//log4cxx::LoggerPtr logger(log4cxx::Logger::getLogger("v3d"));
+
+/**
+ * Global Test Fixture used for initializing the logger
+ */
+struct LogInitializerFixture
+{
+	LogInitializerFixture()
+	{
+		log4cxx::BasicConfigurator::configure();
+	}
+};
+
+BOOST_GLOBAL_FIXTURE(LogInitializerFixture)
